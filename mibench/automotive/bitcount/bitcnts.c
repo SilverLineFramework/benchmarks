7c5d683f885f0c79613e382287efdafe870ee409
Tianshu <thetianshuhuang@gmail.com>
2022-07-06 17:35:05 -0400

    Added mibench/automotive

---
+++ mibench/automotive/bitcount/bitcnts.c
@@ -0,0 +1,67 @@
+/* +++Date last modified: 05-Jul-1997 */
+
+/*
+**  BITCNTS.C - Test program for bit counting functions
+**
+**  public domain by Bob Stout & Auke Reitsma
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "conio.h"
+#include <limits.h>
+#include <time.h>
+#include <float.h>
+#include "bitops.h"
+#include "../../../common/runtime.h"
+
+#define FUNCS  7
+
+static int CDECL bit_shifter(long int x);
+
+int benchmark_main(int argc, char *argv[])
+{
+  clock_t start, stop;
+  double ct, cmin = DBL_MAX, cmax = 0;
+  int i, cminix, cmaxix;
+  long j, n, seed;
+  int iterations = 100000;
+  static int (* CDECL pBitCntFunc[FUNCS])(long) = {
+    bit_count,
+    bitcount,
+    ntbl_bitcnt,
+    ntbl_bitcount,
+    /*            btbl_bitcnt, DOESNT WORK*/
+    BW_btbl_bitcount,
+    AR_btbl_bitcount,
+    bit_shifter
+  };
+  static char *text[FUNCS] = {
+    "Optimized 1 bit/loop counter",
+    "Ratko's mystery algorithm",
+    "Recursive bit count by nybbles",
+    "Non-recursive bit count by nybbles",
+    /*            "Recursive bit count by bytes",*/
+    "Non-recursive bit count by bytes (BW)",
+    "Non-recursive bit count by bytes (AR)",
+    "Shift and count bits"
+  };
+  for (i = 0; i < FUNCS; i++) { 
+    for (j = n = 0, seed = rand(); j < iterations; j++, seed += 13)
+	 n += pBitCntFunc[i](seed);
+  }
+  return 0;
+}
+
+static int CDECL bit_shifter(long int x)
+{
+  int i, n;
+  
+  for (i = n = 0; x && (i < (sizeof(long) * CHAR_BIT)); ++i, x >>= 1)
+    n += (int)(x & 1L);
+  return n;
+}
+
+int main(int argc, char **argv) {
+  return loop(argc, argv, &benchmark_main);
+}
---
+++ mibench/automotive/qsort/qsort_large.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "../../../common/runtime.h"
+
+#define UNLIMIT
+#define MAXARRAY 60000 /* this number, if too large, will cause a seg. fault!! */
+
+struct my3DVertexStruct {
+  int x, y, z;
+  double distance;
+};
+
+int compare(const void *elem1, const void *elem2)
+{
+  /* D = [(x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2]^(1/2) */
+  /* sort based on distances from the origin... */
+
+  double distance1, distance2;
+
+  distance1 = (*((struct my3DVertexStruct *)elem1)).distance;
+  distance2 = (*((struct my3DVertexStruct *)elem2)).distance;
+
+  return (distance1 > distance2) ? 1 : ((distance1 == distance2) ? 0 : -1);
+}
+
+
+int
+benchmark_main(int argc, char *argv[]) {
+  struct my3DVertexStruct *array = malloc(MAXARRAY * sizeof(struct my3DVertexStruct));
+  FILE *fp;
+  int i,count=0;
+  int x, y, z;
+
+    fp = fopen("data/mibench/automotive/input_large.dat","r");
+
+    while((fscanf(fp, "%d", &x) == 1) && (fscanf(fp, "%d", &y) == 1) && (fscanf(fp, "%d", &z) == 1) &&  (count < MAXARRAY)) {
+	 array[count].x = x;
+	 array[count].y = y;
+	 array[count].z = z;
+	 array[count].distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+	 count++;
+    }
+
+  qsort(array,count,sizeof(struct my3DVertexStruct),compare);
+  
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+   return loop(argc, argv, &benchmark_main);
+}
