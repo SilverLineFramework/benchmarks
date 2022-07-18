f4bc9f0670d60057dca75f1eb801586c76b8f5e5
Tianshu <tianshu2@andrew.cmu.edu>
2022-07-18 16:45:12 -0400

    Finished adding vision

---
+++ vision/svm/src/script_svm.c
@@ -0,0 +1,156 @@
+/********************************
+Author: Sravanthi Kota Venkata
+********************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "svm.h"
+#include "../../../common/runtime.h"
+
+int benchmark_main(int argc, char* argv[])
+{
+    int iter, N, Ntst, i, j, k, n;
+    F2D* trn1, *tst1, *trn2, *tst2, *Yoffset;
+    alphaRet* alpha;
+    F2D *a_result, *result;
+    F2D *s;
+    F2D *b_result;
+    F2D *Xtst, *Ytst;
+    char im1[256];
+    int dim = 256;
+
+    N = 100;
+    Ntst = 100;
+    iter = 10;
+
+    #ifdef test
+    N = 4;
+    Ntst = 4;
+    iter = 2;
+    #endif
+    
+    #ifdef sim_fast
+    N = 20;
+    Ntst = 20;
+    iter = 2;
+    #endif
+
+    #ifdef sim
+    N = 16;
+    Ntst = 16;
+    iter = 8;
+    #endif
+    
+    #ifdef sqcif
+    N = 60;
+    Ntst = 60;
+    iter = 6;
+    #endif
+    
+    #ifdef qcif
+    N = 72;
+    Ntst = 72;
+    iter = 8;
+    #endif
+    
+    #ifdef vga
+    N = 450;
+    Ntst = 450;
+    iter = 15;
+    #endif
+    
+    #ifdef wuxga
+    N = 1000;
+    Ntst = 1000;
+    iter = 20;
+    #endif
+
+    iter /= 2;
+    N /= 2;
+
+    #if SIZE == 0
+    char base[100] = "data/vision/svm/sqcif";
+    #elif SIZE == 1
+    char base[100] = "data/vision/svm/qcif";
+    #else
+    char base[100] = "data/vision/svm/cif";
+    #endif
+
+    sprintf(im1, "%s/d16trn_1.txt", base);
+    trn1 = readFile(im1);   
+
+    sprintf(im1, "%s/d16trn_2.txt", base);
+    trn2 = readFile(im1);   
+
+    sprintf(im1, "%s/d16tst_1.txt", base);
+    tst1 = readFile(im1);   
+
+    sprintf(im1, "%s/d16tst_2.txt", base);
+    tst2 = readFile(im1);       
+
+    alpha = getAlphaFromTrainSet(N, trn1, trn2, iter);
+    a_result = alpha->a_result;
+    b_result = alpha->b_result;
+    Yoffset = fSetArray(iter, N, 0);
+
+    Xtst = usps_read_partial(tst1, tst2, -1, 1, Ntst/iter, iter);
+    Ytst = usps_read_partial(tst1, tst2, -1, 0, Ntst/iter, iter);
+
+    for(i=0; i<iter; i++)
+    {
+        F2D *temp;
+        temp = usps_read_partial(trn1, trn2, i, 0, N/iter, iter);
+        for(j=0; j<N; j++)
+            subsref(Yoffset,i,j) = asubsref(temp,j);
+        fFreeHandle(temp);
+    }
+
+
+    result = fSetArray(Ntst,1,0);
+    for( n=0; n<Ntst; n++)
+    {
+        float maxs=0;
+        s=fSetArray(iter,1,0);
+        for( i=0; i<iter; i++)
+        {
+            for (j=0; j<N; j++)
+            {
+                if (subsref(a_result,i,j) > 0)
+                {
+                    F2D *Xtemp, *XtstTemp, *X;
+                    X = alpha->X;
+                    Xtemp = fDeepCopyRange(X,j,1,0,X->width);
+                    XtstTemp = fDeepCopyRange(Xtst, n,1,0,Xtst->width);
+                    asubsref(s,i) = asubsref(s,i) + subsref(a_result,i,j) * subsref(Yoffset,i,j) * polynomial(3,Xtemp,XtstTemp, dim);
+                    fFreeHandle(Xtemp); 
+                    fFreeHandle(XtstTemp); 
+                }
+            }
+            asubsref(s,i) = asubsref(s,i) - asubsref(b_result,i);
+            if( asubsref(s,i) > maxs)
+                maxs = asubsref(s,i);
+        }
+        
+        fFreeHandle(s);
+        asubsref(result,n) = maxs;
+    }
+
+    fFreeHandle(trn1);
+    fFreeHandle(tst1);
+    fFreeHandle(trn2);
+    fFreeHandle(tst2);
+    fFreeHandle(Yoffset);
+    fFreeHandle(result);
+    fFreeHandle(alpha->a_result);
+    fFreeHandle(alpha->b_result);
+    fFreeHandle(alpha->X);
+    free(alpha);
+    fFreeHandle(Xtst);
+    fFreeHandle(Ytst);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+   return loop(argc, argv, &benchmark_main);
+}
---
+++ vision/texture/src/script_texture_synthesis.c
@@ -0,0 +1,178 @@
+/********************************
+Author: Sravanthi Kota Venkata
+********************************/
+
+#include "texture.h"
+#include "../../../common/runtime.h"
+
+int WIDTHin,HEIGHTin;
+F2D *target, *result;
+int WIDTH,HEIGHT;
+int localx, localy,targetin=0;
+int *atlas;
+int *xloopin, *yloopin;
+int *xloopout, *yloopout;
+
+int  benchmark_main(int argc, char **argv)
+{
+    params* data;
+    I2D *im;
+    F2D* image;
+    unsigned int *start, *end, *elapsed;
+
+    data = malloc(sizeof(params));
+    im = parse_flags(argc, argv);
+    image = fiDeepCopy(im);
+    init_params(data);
+
+    create_texture(image, data);
+    
+    iFreeHandle(im);
+    fFreeHandle(image);
+    free(data);
+
+    fFreeHandle(target);
+    fFreeHandle(result);
+    free(atlas);
+//    free(xloopout);
+//    free(yloopout);
+    
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+   return loop(argc, argv, &benchmark_main);
+}
+
+I2D* parse_flags(int argc, char ** argv)
+{
+    int i, tsx,tsy;
+    I2D* image;
+
+    #if SIZE == 0
+    char fileNm[256] = "data/vision/texture/sqcif/1.bmp";
+    #elif SIZE == 1
+    char fileNm[256] = "data/vision/texture/qcif/1.bmp";
+    #else
+    char fileNm[256] = "data/vision/texture/cif/1.bmp";
+    #endif
+
+    image = readImage(fileNm);
+    WIDTHin = image->width;
+    HEIGHTin = image->height;
+
+    localx = 3;
+    localy = 3;
+
+#ifdef test
+    WIDTH = WIDTHin*2;
+    HEIGHT = HEIGHTin*2;
+    localx = 2;
+    localy = 2;
+#endif
+#ifdef sim_fast
+    WIDTH = WIDTHin*2;
+    HEIGHT = HEIGHTin*2;
+    localx = 3;
+    localy = 3;
+#endif
+#ifdef sim
+    WIDTH = WIDTHin*3;
+    HEIGHT = HEIGHTin*3;
+    localx = 2;
+    localy = 2;
+#endif
+#ifdef sqcif
+    WIDTH = WIDTHin*6;
+    HEIGHT = HEIGHTin*6;
+    localx = 2;
+    localy = 2;
+#endif
+#ifdef qcif
+    WIDTH = WIDTHin*10;
+    HEIGHT = HEIGHTin*10;
+    localx = 2;
+    localy = 2;
+#endif
+#ifdef cif
+    WIDTH = WIDTHin*10;
+    HEIGHT = HEIGHTin*10;
+    localx = 3;
+    localy = 3;
+#endif
+#ifdef vga
+    WIDTH = WIDTHin*20;
+    HEIGHT = HEIGHTin*20;
+    localx = 3;
+    localy = 3;
+#endif
+#ifdef fullhd
+    WIDTH = WIDTHin*20;
+    HEIGHT = HEIGHTin*20;
+    localx = 15;
+    localy = 15;
+#endif
+#ifdef wuxga
+    WIDTH = WIDTHin*20;
+    HEIGHT = HEIGHTin*20;
+    localx = 5;
+    localy = 5;
+#endif
+
+    result = fMallocHandle(1,HEIGHT*WIDTH);
+    target = fMallocHandle(1, WIDTH*HEIGHT);
+ 
+    atlas = malloc(2*WIDTH*HEIGHT*sizeof(int));
+    xloopout = malloc(2*WIDTH*sizeof(int));
+    yloopout = malloc(2*HEIGHT*sizeof(int));
+    
+    for(i=-WIDTH/2;i<WIDTH+WIDTH/2;i++)
+    {
+        arrayref(xloopout,i+WIDTH/2) = (WIDTH+i)%WIDTH;
+    }
+    for(i=-HEIGHT/2;i<HEIGHT+HEIGHT/2;i++)
+    {
+        arrayref(yloopout,i+HEIGHT/2) = (HEIGHT+i)%HEIGHT;
+    }
+    xloopout += WIDTH/2; yloopout += HEIGHT/2;
+
+    if (result == NULL)
+    {
+        printf("Can't allocate %dx%d image. Exiting.\n",WIDTH,HEIGHT);
+        exit(1);
+    }
+
+    return image;
+}
+
+void init_params(params *data)
+{
+    int i,j;
+    data->localx = localx; data->localy = localy;
+    data->widthin = WIDTHin; data->widthout = WIDTH;
+    data->heightin = HEIGHTin; data->heightout = HEIGHT;
+
+
+    if(!targetin)
+    {
+        for(i=0;i<data->heightout;i++)
+        {
+            for(j=0;j<data->widthout;j++)
+            {
+                asubsref(target,a(j,i,data->widthout)+R) = 1.0;
+//                asubsref(target,a(j,i,data->widthout)+G) = 1.0;
+//                asubsref(target,a(j,i,data->widthout)+B) = 1.0;
+            }
+        }
+    }
+ 
+    for(i=0;i<data->heightout;i++)
+    {
+        for(j=0;j<data->widthout;j++)
+        {
+            asubsref(result,a(j,i,data->widthout)+R)  = 1.0;
+//            asubsref(result,a(j,i,data->widthout)+G)  = 1.0;
+//            asubsref(result,a(j,i,data->widthout)+B)  = 1.0;
+        }
+    }
+}
