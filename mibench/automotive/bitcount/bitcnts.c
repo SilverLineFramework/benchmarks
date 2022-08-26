6b0a1b170f90b495f065d01ca83463d01ffb1dd4
Tianshu <tianshu2@andrew.cmu.edu>
2022-08-26 13:31:05 -0400

    Revert iterations again (with note)

--- mibench/automotive/bitcount/bitcnts.c
+++ mibench/automotive/bitcount/bitcnts.c
@@ -23,7 +23,8 @@ int benchmark_main(int argc, char *argv[])
   double ct, cmin = DBL_MAX, cmax = 0;
   int i, cminix, cmaxix;
   unsigned long j, n, seed;
-  int iterations = 1000000;
+  // Cannot increase iterations due to unknown overflow bug!
+  int iterations = 100000;
   static int (* CDECL pBitCntFunc[FUNCS])(unsigned long) = {
     bit_count,
     bitcount,
